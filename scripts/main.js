d9b2b3cb239988488b41296d3dd9b7a94eaffb68
Anca Musat <ank17_2003@yahoo.com>
2021-01-19 23:30:29 +0200

    Added some spaces between/after operators, removed the double quotes marks

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,16 @@
 let dictionary = [];
-let containsWord;
 
 function addWord() {
-    let aw=document.getElementById('addW').value;
-    if (dictionary.indexOf(aw)===-1) {
+    let aw = document.getElementById('addW').value;
+    if (dictionary.indexOf(aw) === -1) {
         dictionary.push(aw);
-        alert("The word "+aw+" was added to the dictionary");  
+        alert('The word '+ aw + ' was added to the dictionary');  
     } else {
-        alert('The word '+aw+' already exists in the dictionary');
+        alert('The word ' + aw+ ' already exists in the dictionary');
     }
 }
 
 function searchWord() {
     let sw = document.getElementById('searchW').value;
-    containsWord= (dictionary.indexOf(sw)===-1) ? (alert("The dictionary does not contain the word "+sw)) : (alert("The dictionary contains the word "+sw));
+    let containsWord = (dictionary.indexOf(sw) === -1) ? (alert('The dictionary does not contain the word '+ sw)) : (alert('The dictionary contains the word ' + sw));
 }
