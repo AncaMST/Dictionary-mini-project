1941d6ed24546e567f2e605b9f7e039c3408dcaf
Anca Musat <ank17_2003@yahoo.com>
2021-05-08 13:52:43 +0300

    Added single quotation marks and the functionality onfocus for input

--- scripts/main.js
+++ scripts/main.js
@@ -4,13 +4,13 @@ function addWord() {
     let aw = document.getElementById('addW').value;
     if (dictionary.indexOf(aw) === -1) {
         dictionary.push(aw);
-        alert('The word '+ aw + ' was added to the dictionary');  
+        alert('The word \''+ aw + '\' was added to the dictionary');  
     } else {
-        alert('The word ' + aw+ ' already exists in the dictionary');
+        alert('The word \'' + aw+ '\' already exists in the dictionary');
     }
 }
 
 function searchWord() {
     let sw = document.getElementById('searchW').value;
-    let containsWord = (dictionary.indexOf(sw) === -1) ? (alert('The dictionary does not contain the word '+ sw)) : (alert('The dictionary contains the word ' + sw));
+    let containsWord = (dictionary.indexOf(sw) === -1) ? (alert('The dictionary does not contain the word \''+ sw + '\'')) : (alert('The dictionary contains the word \'' + sw + '\''));
 }
